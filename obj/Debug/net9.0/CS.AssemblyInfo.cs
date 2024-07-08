77aefccdb4f97658952085648bb56e57788ee70c
md shahreer irfan <mdshahreerirfan@gmail.com>
2024-07-09 01:22:26 +0600

    Hello

--- obj/Debug/net9.0/CS.AssemblyInfo.cs
+++ obj/Debug/net9.0/CS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c2f2258eb626f00c79dd12761c5f29f7bde4b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+311750c9578652e910a4d5e1d14fd6f22f0b63a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
